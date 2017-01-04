7e7e11fee0bca1a2995fb9f2f00f42b7e6b1d05e
Neo Shan <neoimale@gmail.com>
2017-01-04 15:01:00 +0800

    fix login verify

--- router/login.js
+++ router/login.js
@@ -52,7 +52,7 @@ router.post('*', function(req, res) {
         })
 })
 
-router.get('verify', function(req, res) {
+router.get('/verify', function(req, res) {
     if (!req.query.sessionid) {
         res.endj({
             code: -1,
